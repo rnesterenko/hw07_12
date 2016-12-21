1b2f01c12e96f577878e20d1dbbb0bf550fdce38
rnesterenko <r_nesterenko@i.ua>
2016-12-21 13:57:19 +0200

    initial commit

---
+++ file4.php
@@ -0,0 +1,74 @@
+<?php
+?>
+
+<?
+
+include("header.php");
+
+?>
+
+<?
+
+include("footer.php");
+
+?>
+
+<?php
+
+$master ="Скорая помощь клиенту";
+
+?>
+<?php
+
+if ($master == "Скорая помощь клиенту")
+{
+
+    echo "<p>Кто-нибудь сделайте что-нибудь</p>";
+
+}
+
+else
+
+{
+
+    echo "<p><a href=\"file4.php\" title=\"Скорая помощь клиенту\">Кто-нибудь сделайте что-нибудь</a></h3></p>";
+
+}
+?>
+
+
+<html>
+
+<head>
+
+    <meta charset="UTF-8">
+    <title>Crocodile-MINI</title>
+    <style>
+        P {
+            text-indent: 20px;
+            text-align: center;
+            color: rgb(13, 20, 151);
+        }
+        .letter {
+            color: #051297;
+            font-size: 200%;
+        }
+    </style>
+
+</head>
+
+<body>
+
+<body bgcolor="#f0fff0">
+<h3 align="center"><a href="index.php" title="Главная страницаи">Главная</a>|
+    <a href="file1.php" title="Отдать нам свои деньги">Буду клиентом</a>|
+    <a href="file2.php" title="Операции со своими крокодилами">Распоряжение своими крокодилами</a>|
+    <a href="file3.php" title="Рассчитаться за покупку">Операции он-лайн</a>
+
+
+
+<p><img src="1.jpg" alt="Let's go!"></p>
+
+</body>
+
+</html>
